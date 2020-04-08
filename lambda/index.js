7477db2aaef8f41fb9dc8a46abf6648e6e07be0b
Bob Jones <jamjones@redhat.com>
2020-04-08 18:17:16 -0400

    Initial commit

---
+++ lambda/index.js
@@ -0,0 +1,252 @@
+// ASK SDK
+const Alexa = require('ask-sdk-core');
+// i18n library dependency, we use it below in a localisation interceptor
+const i18n = require('i18next');
+// We import a language strings object containing all of our strings.
+// The keys for each string will then be referenced in our code, e.g. handlerInput.t('WELCOME_MSG')
+const languageStrings = require('./languageStrings');
+// We will use the moment.js package in order to make sure that we calculate the date correctly
+const moment = require('moment-timezone');
+const axios = require('axios');
+
+/////////////////////////////////
+// Handlers Definition
+/////////////////////////////////
+const TeamWinnerPredictionIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'TeamWinnerPredictionIntent';
+    },
+    
+    async fetchPrediction(sport, teamA, teamB) {
+        const serviceUrl = `http://hackfest-alexa-bookie-alexa.apps.cluster-fed2.fed2.example.opentlc.com/api/v1/bookie/${sport}/${teamA}/${teamB}`;
+        //const serviceUrl = `http://hackfest-alexa-bookie-alexa.apps.cluster-fed2.fed2.example.opentlc.com/api/v1/bookie/nfl/carolina/denver`;
+        
+        console.log(serviceUrl);
+        
+        let result = {};
+        
+        try {
+            result = await axios.get(serviceUrl, {
+                timeout: 6500,
+                headers: { "Accept": "application/json" }
+            
+            });
+        } catch(e) { 
+            console.log("error: " + e);
+        }
+        
+        console.log(result);
+        return result;
+    },
+    
+    async handle(handlerInput) {
+        const { requestEnvelope } = handlerInput;
+        
+        console.log("Sport ID" + requestEnvelope.request.intent.slots.sport.resolutions.resolutionsPerAuthority[0].values[0].value.id);
+
+        const teamA = Alexa.getSlotValue(requestEnvelope, 'team_a');
+        const teamB = Alexa.getSlotValue(requestEnvelope, 'team_b');
+        const sport = Alexa.getSlotValue(requestEnvelope, 'sport');
+        
+        
+        const predictionResult = await this.fetchPrediction(sport.toLowerCase(), teamA, teamB);
+        console.log("Prediction Results: " + predictionResult);
+        
+        let winningTeam = teamA;
+        let speakOutput;
+        
+        if (predictionResult.data) {
+            if (predictionResult.data.home_score < predictionResult.data.away_score) {
+                winningTeam = predictionResult.data.away_team;
+            } else {
+                winningTeam = predictionResult.data.home_team;
+            }
+            speakOutput = handlerInput.t('PREDICT_MESSAGE', { team: winningTeam });
+        } else {
+            speakOutput = handlerInput.t('PREDICT_HARD_MESSAGE', { team: winningTeam });
+        }
+    
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+/**
+ * Handles LaunchRequest requests sent by Alexa when no birthdate has been registered
+ * Note : this type of request is send when the user invokes your skill without providing a specific intent.
+ */
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speakOutput = handlerInput.t('WELCOME_MSG');
+        const repromptOutput = handlerInput.t('WELCOME_REPROMPT_MSG');
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(repromptOutput)
+            .getResponse();
+    }
+};
+
+/**
+ * Handles AMAZON.HelpIntent requests sent by Alexa 
+ * Note : this request is sent when the user makes a request that corresponds to AMAZON.HelpIntent intent defined in your intent schema.
+ */
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = handlerInput.t('HELP_MSG');
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+/**
+ * Handles AMAZON.CancelIntent & AMAZON.StopIntent requests sent by Alexa 
+ * Note : this request is sent when the user makes a request that corresponds to AMAZON.CancelIntent & AMAZON.StopIntent intents defined in your intent schema.
+ */
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = handlerInput.t('GOODBYE_MSG');
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+
+/* *
+ * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
+ * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
+ * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
+ * */
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+/* *
+ * The intent reflector is used for interaction model testing and debugging.
+ * It will simply repeat the intent the user said. You can create custom handlers for your intents 
+ * by defining them above, then also adding them to the request handler chain below 
+ * */
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = handlerInput.t('REFLECTOR_MSG', { intentName: intentName });
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+/**
+ * Generic error handling to capture any syntax or routing errors. If you receive an error
+ * stating the request handler chain is not found, you have not implemented a handler for
+ * the intent being invoked or included it in the skill builder below 
+ * */
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`~~~~ Error handled: ${error.message}`);
+        const speakOutput = handlerInput.t('ERROR_MSG');
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+/////////////////////////////////
+// Interceptors Definition
+/////////////////////////////////
+
+/**
+ * This request interceptor will log all incoming requests in the associated Logs (CloudWatch) of the AWS Lambda functions
+ */
+const LoggingRequestInterceptor = {
+    process(handlerInput) {
+        console.log("\n" + "********** REQUEST *********\n" +
+            JSON.stringify(handlerInput, null, 4));
+    }
+};
+
+/**
+ * This response interceptor will log outgoing responses if any in the associated Logs (CloudWatch) of the AWS Lambda functions
+ */
+const LoggingResponseInterceptor = {
+    process(handlerInput, response) {
+        if (response) console.log("\n" + "************* RESPONSE **************\n"
+            + JSON.stringify(response, null, 4));
+    }
+};
+
+/**
+ * This request interceptor will bind a translation function 't' to the handlerInput
+ */
+const LocalisationRequestInterceptor = {
+    process(handlerInput) {
+        i18n.init({
+            lng: Alexa.getLocale(handlerInput.requestEnvelope),
+            resources: languageStrings
+        }).then((t) => {
+            handlerInput.t = (...args) => t(...args);
+        });
+    }
+};
+
+/////////////////////////////////
+// SkillBuilder Definition
+/////////////////////////////////
+
+/**
+ * The SkillBuilder acts as the entry point for your skill, routing all request and response
+ * payloads to the handlers above. Make sure any new handlers or interceptors you've
+ * defined are included below. The order matters - they're processed top to bottom.
+ */
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        TeamWinnerPredictionIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler) // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+    .addErrorHandlers(
+        ErrorHandler)
+    .addRequestInterceptors(
+        LocalisationRequestInterceptor,
+        LoggingRequestInterceptor
+    )
+    .addResponseInterceptors(
+        LoggingResponseInterceptor)
+    .lambda();
---
+++ lambda/languageStrings.js
@@ -0,0 +1,22 @@
+/* *
+ * Language strings object containing all of our strings.
+ * The keys for each string will then be referenced in our code, e.g. handlerInput.t('WELCOME_MSG').
+ * The localisation interceptor in index.js will automatically choose the strings
+ * that match the request's locale.
+ * */
+
+module.exports = {
+    en: {
+        translation: {
+            WELCOME_MSG: `Hello! I am a sport bookie. I can make a prediction on what two sport teams would win. I know NFL, MLB, NHL, CBB, CFB, and NBA. You can just say, "who would win between Arizona and Texas in the NFL" and I will make a prediction. What would you like me to predict?`,
+            WELCOME_REPROMPT_MSG: `Sorry, What would you like me to predict?`,
+            PREDICT_MESSAGE: `That's a good match! I would bet on {{team}}.`,
+            PREDICT_HARD_MESSAGE: `Wow, what a tough match! I would go with {{team}}.`,
+            HELP_MSG: `You can tell me your two teams and I'll tell you who would win. I know NFL, MLB, NHL, CBB, CFB, and NBA. You can just say, "who would win between team 1 and team 2 in the sport" and I will make a prediction. For eample, say "who would win between Carolina and boston in the NHL?". Which one would you like to try?`,
+            GOODBYE_MSG: `Good Luck with your team!`,
+            REFLECTOR_MSG: `You just triggered {{intentName}}`,
+            ERROR_MSG: `Sorry, I couldn't understand what you said. Can you say it again?`,
+            ERROR_TIMEZONE_MSG: `I can't determine your timezone. Please check your device settings and make sure a timezone was selected. After that please reopen the skill and try again!`
+        }
+    }
+}
